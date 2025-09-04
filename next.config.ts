14a41ac62cfd979b458e8ef9b0cd28eeb492d08b
hungdinh1212 <dinhhung12122003@gmai.com>
2025-09-04 12:34:20 +0700

    chore(config): Turn off eslint when building

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   reactStrictMode: false,
+  reactStrictMode: false,
+  eslint: {
+    // Không chạy ESLint khi build (Vercel/production)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
