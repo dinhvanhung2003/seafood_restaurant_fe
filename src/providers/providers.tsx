9fa562c1f7059eba361550ca3d82588253ba41bb
hungdinh1212 <dinhhung12122003@gmai.com>
2025-09-26 16:40:08 +0700

    BTS-75 feat:(query):[FE] Configure gcTime and staleTime for API calls fetch

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -2,12 +2,33 @@
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  const [client] = useState(() => new QueryClient());
+  const [client] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60_000,          // dữ liệu “tươi” 1 phút
+            gcTime: 5 * 60_000,         // giữ cache 5 phút
+            refetchOnWindowFocus: false,
+            retry: 2,
+          },
+          mutations: {
+            retry: 0,
+          },
+        },
+      })
+  );
+
   return (
     <SessionProvider>
-      <QueryClientProvider client={client}>{children}</QueryClientProvider>
+      <QueryClientProvider client={client}>
+        
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
