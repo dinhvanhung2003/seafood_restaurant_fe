299721e1d0553c954915b6960d4810d9df3a2ab0
hungdinh1212 <dinhhung12122003@gmai.com>
2025-10-17 13:04:31 +0700

    deploy:fix path

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
+   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "*.amazonaws.com" },
+      // AWS S3
+      { protocol: 'https', hostname: '*.amazonaws.com' },
+      // Nếu dùng custom endpoint (MinIO / CDN) thì thêm vào đây:
+      // { protocol: 'https', hostname: 'cdn.yourdomain.com' },
+      // { protocol: 'http',  hostname: 'minio.local', port: '9000' },
     ],
   },
   reactStrictMode: false,
   eslint: {
+    // Không chạy ESLint khi build (Vercel/production)
+    
     ignoreDuringBuilds: true,
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://seafoodrestaurantbe-production.up.railway.app/:path*",
-      },
-    ];
   },
 };
 
